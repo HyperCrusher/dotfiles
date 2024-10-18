3c8889de0df73686a3c3cbb4f6eb898ab860a514
Hyper <hypercrusher@proton.me>
2024-10-18 01:59:48 -0700

    [system] Set refresh rate and gsync
    Set my refresh rate to 144 and turn on gstnc for both my monitors

--- nixos/system/machines/home-desktop/wm.nix
+++ nixos/system/machines/home-desktop/wm.nix
@@ -13,6 +13,14 @@
         sxhkd.package = pkgs.sxhkd;
       };
     };
+    monitorSection = ''
+      VertRefresh     48.0 - 165.0
+      Option         "DPMS"
+    '';
+    screenSection = ''
+      Option         "nvidiaXineramaInfoOrder" "DP-4"
+      Option         "metamodes" "DP-4: 2560x1440_144 +0+0 {AllowGSYNCCompatible=On}, DP-2: 2560x1440_144 +2560+0 {AllowGSYNCCompatible=On}"
+    '';
   };
 
   services.displayManager.ly.enable = true;
