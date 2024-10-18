32535683ede2ea24137e6d5d4ee930d7c84788bd
Hyper <hypercrusher@proton.me>
2024-10-18 01:56:00 -0700

    [bug] comments out obs-vkcapture
    obs-vkcapture currently causes nixos to fail to build

--- nixos/home/packages/media/video.nix
+++ nixos/home/packages/media/video.nix
@@ -9,7 +9,7 @@
     obs-cli
     obs-studio-plugins.wlrobs
     obs-studio-plugins.waveform
-    obs-studio-plugins.obs-vkcapture
+    #obs-studio-plugins.obs-vkcapture
     obs-studio-plugins.obs-webkitgtk
     obs-studio-plugins.obs-mute-filter
     obs-studio-plugins.obs-source-clone
