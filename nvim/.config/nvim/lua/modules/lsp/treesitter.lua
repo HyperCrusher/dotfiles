985e2d22ba0a658a45c39ea5315ea5c3be32dc9e
Hyper <hypercrusher@proton.me>
2026-01-20 23:50:41 -0700

    Update treesitter config
    I love breaking changes

--- nvim/.config/nvim/lua/modules/lsp/treesitter.lua
+++ nvim/.config/nvim/lua/modules/lsp/treesitter.lua
@@ -1,91 +1,47 @@
-local function ensureInstalled()
-  return {
-    "asm", "bash", "c", "cmake", "comment", "commonlisp", "cpp", "css",
-    "dockerfile", "disassembly", "gdscript", "gdshader", "glsl", "go",
-    "godot_resource", "gomod", "gosum", "gowork", "gpg", "gitignore", "haskell",
-    "hlsl", "html", "http", "hyprlang", "ini", "java", "javascript", "jq", "jsdoc",
-    "json5", "kotlin", "latex", "lua", "make", "markdown", "markdown_inline",
-    "rust", "scss", "slint", "styled", "tmux", "toml", "tsx", "typescript",
-    "typst", "wgsl", "wgsl_bevy", "yaml", "yuck", "zig"
-  }
-end
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    init = function(plugin)
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
     dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
-      { "windwp/nvim-ts-autotag" },
-      { "RRethy/nvim-treesitter-endwise" },
-      { "m-demare/hlargs.nvim" },
+      "windwp/nvim-ts-autotag",
+      "RRethy/nvim-treesitter-endwise",
+      "m-demare/hlargs.nvim",
     },
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = ensureInstalled(),
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["as"] = "@scope",
-          },
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@function.outer"] = "V",
-            ["@class.outer"] = "V",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<Leader>sn"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<Leader>sp"] = "@parameter.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-            ["]s"] = "@scope",
-            ["]z"] = "@fold",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-            ["[s"] = "@scope",
-            ["[z"] = "@fold",
-          },
-          goto_next = {
-            ["]]"] = "@conditional.outer",
-          },
-          goto_previous = {
-            ["[["] = "@conditional.outer",
-          },
-        },
-      },
-      endwise = {
-        enable = true,
-      },
-      autotag = {
-        enable = true,
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+    config = function()
+      local ensureInstalled = {
+        "asm", "bash", "c", "cmake", "comment", "commonlisp", "cpp", "css",
+        "dockerfile", "disassembly", "gdscript", "gdshader", "glsl", "go",
+        "godot_resource", "gomod", "gosum", "gowork", "gpg", "gitignore", "haskell",
+        "hlsl", "html", "http", "hyprlang", "ini", "java", "javascript", "jq", "jsdoc",
+        "json5", "kotlin", "latex", "lua", "make", "markdown", "markdown_inline",
+        "rust", "scss", "slint", "styled", "tmux", "toml", "tsx", "typescript",
+        "typst", "wgsl", "wgsl_bevy", "yaml", "yuck", "zig"
+      }
+      local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+      local parsersToInstall = vim.iter(ensureInstalled)
+          :filter(function(parser) return not vim.tbl_contains(alreadyInstalled, parser) end)
+          :totable()
+      require("nvim-treesitter").install(parsersToInstall)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { '*' },
+        callback = function()
+          pcall(vim.treesitter.start)
+          -- folds
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo.foldmethod = 'expr'
+          -- indents
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      vim.g.no_plugin_maps = true
+    end,
+    config = true,
+  }
 }
